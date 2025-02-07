3d3ed7f6f498af624e8237fd484eaf7a984de79d
Megh39 <meghnanavati1@gmail.com>
2025-02-07 17:25:56 +0530

    cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Apply cors AFTER setting headers
 app.use(cors({
-    origin: "https://college-review-khaki.vercel.app",
+    origin: "https://college-review-khaki.vercel.app/",
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type, Authorization",
 
