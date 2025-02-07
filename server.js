05fe6610105f8408c788a5a67f9f9eef99b5b373
Megh39 <meghnanavati1@gmail.com>
2025-02-07 17:30:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.use(cors({
     credentials: true
 }));
 
+// ✅ Manually set CORS headers (extra security)
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://college-review-khaki.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 // Connect to MongoDB
 connectDB();
 
