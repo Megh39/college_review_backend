0820706607bc0dd408ebe26e34e8c68162d4c2ec
Megh39 <meghnanavati1@gmail.com>
2025-02-07 17:18:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+    origin: "https://college-review-khaki.vercel.app", // Correct frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
 // Connect to MongoDB
 connectDB();
 
