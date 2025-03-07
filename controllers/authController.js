fae5d3970c0ac7e02d348ca86eb79e6397de8333
Megh39 <meghnanavati1@gmail.com>
2025-03-07 23:39:21 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const Admin=require("../models/Admin")
+const Admin = require("../models/Admin")
 // Register User
 const registerUser = async (req, res) => {
-    const { username, email, password, age,college_name,course } = req.body;
+    const { username, email, password, age, college_name, course } = req.body;
 
     if (!username || !email || !password || !age) {
         return res.status(400).json({ message: "All fields are required" });
@@ -16,7 +16,7 @@ const registerUser = async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword, age });
+        const newUser = new User({ username, email, password: hashedPassword, age, college_name, course });
         await newUser.save();
 
         res.status(201).json({ message: "User registered successfully", user: newUser });
@@ -36,7 +36,7 @@ const loginUser = async (req, res) => {
         let role = "user";
 
         if (username) {
-            user = await Admin.findOne({ username }); 
+            user = await Admin.findOne({ username });
             if (user) {
                 if (user.password !== password) {
                     return res.status(401).json({ message: "Invalid admin password" });
@@ -57,9 +57,9 @@ const loginUser = async (req, res) => {
             }
         }
 
-        res.status(200).json({ 
-            message: "Login successful", 
-            user: { ...user._doc, role } 
+        res.status(200).json({
+            message: "Login successful",
+            user: { ...user._doc, role }
         });
 
     } catch (error) {
@@ -69,7 +69,7 @@ const loginUser = async (req, res) => {
 // Get All Users
 const getAllUsers = async (req, res) => {
     try {
-        const users = await User.find().select("-password"); 
+        const users = await User.find().select("-password");
         res.json(users);
     } catch (error) {
         res.status(500).json({ message: "Server error", error: error.message });
