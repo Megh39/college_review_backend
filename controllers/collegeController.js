6bc937d4cf65cdb05ee62045cd3721713e559cb8
Megh39 <meghnanavati1@gmail.com>
2025-03-18 00:26:11 +0530

    updated collection

--- controllers/collegeController.js
+++ controllers/collegeController.js
@@ -1,18 +1,5 @@
-const mongoose = require('mongoose');
 const College = require('../models/College');
 
-// Connect to MongoDB (You can call this function in your main server file)
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://localhost:27017/college_review_db', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB:', err);
-    }
-};
 
 // Fetch all colleges
 const getAllColleges = async (req, res) => {
@@ -31,7 +18,7 @@ const getCollegeById = async (req, res) => {
     try {
         const college = await College.findOne({ id });
         if (!college) return res.status(404).json({ message: "College not found" });
-        
+
         res.json(college);
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch college", error: error.message });
--- models/College.js
+++ models/College.js
@@ -6,5 +6,5 @@ const collegeSchema = new mongoose.Schema({
     google_map_link: String,
     college_facilities: mongoose.Schema.Types.Mixed,
     courses: mongoose.Schema.Types.Mixed
-});
+}, { collection: "all_college_details" });
 module.exports = mongoose.model("College", collegeSchema);
